9ea899f52e499807b68ba66629a49ddcf18b507d
rgranatodutra <r.granatodutra@gmail.com>
2023-05-28 15:20:24 -0300

    feat: added conditional to choose repository

--- src/modules/songs/songs.module.ts
+++ src/modules/songs/songs.module.ts
@@ -3,14 +3,17 @@ import { SongsService } from './songs.service';
 import { SongsController } from './songs.controller';
 import { SongsRepository } from './repositories/songs.repository';
 import { SongsInMemoryRepository } from './repositories/songs.memory.repository';
+import { SongsPrismaRepository } from './repositories/songs.prisma.repository';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
   controllers: [SongsController],
   providers: [
     SongsService,
+    PrismaService,
     {
       provide: SongsRepository,
-      useClass: SongsInMemoryRepository
+      useClass: process.env.USE_PRISMA === "TRUE" ? SongsPrismaRepository : SongsInMemoryRepository
     }
   ]
 })
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,15 +3,18 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './repositories/users.repository';
 import { UsersInMemoryRepository } from './repositories/users.memory.repository';
+import { UsersPrismaRepository } from './repositories/users.prisma.repository';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
   controllers: [UsersController],
   providers: [
     UsersService,
+    PrismaService,
     {
       provide: UsersRepository,
-      useClass: UsersInMemoryRepository
+      useClass: process.env.USE_PRISMA === "TRUE" ? UsersPrismaRepository : UsersInMemoryRepository
     }
   ]
 })
-export class UsersModule {};
+export class UsersModule { };
